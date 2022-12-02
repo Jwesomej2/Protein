6fa35589ddf5d4e619b7a4989d27eeb5dcd9b005
Jwesomej2 <112572043+Jwesomej2@users.noreply.github.com>
2022-12-01 23:15:57 -0600

    Update SetGoalForm.cs

--- SetGoalForm.cs
+++ SetGoalForm.cs
@@ -19,6 +19,27 @@ namespace WindowsFormsApp1
 
         private void SetGoalForm_Load(object sender, EventArgs e)
         {
+        {
+		/// <summary>
+		///   When the food was consumed.
+		/// </summary>
+		public DateTime When { get; set; }
+
+		/// <summary>
+		///   How much of the food was consumed.
+		/// </summary>
+		/// <remarks>
+		///   This is in the food's reference size units, e.g. "25" for 25 grams of chocolate (per 100g),
+		///   or "2" for 2 eggs.
+		/// </remarks>
+		public float Amount { get; set; }
+
+		/// <summary>
+		///   What food was eaten.
+		/// </summary>
+		public Food Food { get; set; }
+	}
+}
 
         }
     }
